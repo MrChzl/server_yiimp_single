746d091c15b8edf451bc10874f1324944184b619
MrChzl <LifeEscalade@gmail.com>
2022-03-12 07:49:33 -0500

    7.3

--- system.sh
+++ system.sh
@@ -157,8 +157,8 @@ apt_install php7.3-fpm php7.3-opcache php7.3-fpm php7.3 php7.3-common php7.3-gd 
 php7.3-mysql php7.3-imap php7.3-cli php7.3-cgi \
 php-pear php-auth-sasl mcrypt imagemagick libruby \
 php7.3-curl php7.3-intl php7.3-pspell php7.3-recode php7.3-sqlite3 \
-php7.3-tidy php7.3-xmlrpc php7.3-xsl php7.3-memcache memcached php-memcache \
-php-imagick php-gettext php7.3-zip php7.3-mbstring \
+php7.3-tidy php7.3-xmlrpc php7.3-xsl php7.3-memcache memcached php7.3-memcached \
+php7.3-imagick php7.3-gettext php7.3-zip php7.3-mbstring \
 fail2ban ntpdate python3 python3-dev python3-pip \
 curl git sudo coreutils pollinate unzip unattended-upgrades cron \
 pwgen libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev \
